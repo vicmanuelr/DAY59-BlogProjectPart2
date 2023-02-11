d1049ea1a693a22c54625867bfcec36d945a6d54
Victor Ponce <vicmanuelr@gmail.com>
2023-02-11 16:36:31 -0600

    main.py edited added api calls and automatic year update from datetime
    - created call_posts_api function
    - removed route /index.html since not referenced anymore
    - added blog title/subtitle in index.html
    - added year parms and all posts to pass to index.html

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template, url_for
+from datetime import datetime
+import requests
 
 
 app = Flask(__name__)
 
 
+year = datetime.now().year
+
+
+def call_posts_api():
+    blog_url = "https://api.npoint.io/8cb7d88de0d7d2c9ca8f"
+    response = requests.get(url=blog_url)
+    all_posts = response.json()
+    return all_posts
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-
-@app.route('/index.html')
-def index():
-    return render_template("index.html")
+    all_posts = call_posts_api()
+    return render_template("index.html", year=year, posts=all_posts)
 
 
 @app.route("/about")
