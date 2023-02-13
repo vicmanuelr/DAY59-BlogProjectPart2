71e5685b57dc2e4f04230d35673cc0b19299b8cc
Victor Ponce <vicmanuelr@gmail.com>
2023-02-13 17:28:55 -0600

    Added post.html route and passed in selected post from index
    - added article on post template
    - pending background image on each template

--- main.py
+++ main.py
@@ -32,5 +32,15 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/blog/<num>")
+def get_blog(num):
+    all_posts =call_posts_api()
+    post_selected = {}
+    for post in all_posts:
+        if int(post["id"]) == int(num):
+            post_selected = post
+    return render_template("post.html", year=year, post=post_selected)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
